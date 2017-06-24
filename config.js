046c4978441a66795b56048d4f71b46975c41ba1
Luc Bronsdijk <info@lucbronsdijk.nl>
2017-06-24 12:39:13 +0200

    Add Slack Hook uri

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ var config = {};
 config.port = process.env.PORT || 3000;
 config.clientId = process.env.CLIENT_ID;
 config.clientSecret = process.env.CLIENT_SECRET;
+config.slackHookUri = "https://hooks.slack.com/services/T5WFP7QS3/B5YJF456W/" + process.env.SLACK_HOOK_SECRET;
 
 module.exports = config;
