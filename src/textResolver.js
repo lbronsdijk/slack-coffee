1c4e0e9e307f1fbfff18f9e48f125d9e630c1d3b
Luc Bronsdijk <info@lucbronsdijk.nl>
2017-06-24 13:36:51 +0200

    Add text resolver - Resolves random text for different temperatures

---
+++ src/textResolver.js
@@ -0,0 +1,19 @@
+module.exports = {
+    temperature: function (temp) {
+        var text;
+
+        if (temp > 80) {
+            text = "Your coffee is way to hot. Blow before drinking!";
+        }
+
+        if (temp >= 60 && temp <= 80) {
+            text = "Your coffee is perfect to drink.";
+        }
+
+        if (temp < 60) {
+            text = "Your coffee becomes a Ice Coffee.";
+        }
+
+        return ":coffee: " + text;
+    }
+};
